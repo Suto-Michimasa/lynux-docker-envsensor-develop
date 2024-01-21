68ae53c869dbd992ec4972b8bdff9e4df52fb195
Suto <sutou1335michimasa@gmail.com>
2024-01-21 21:45:58 +0900

    init next.js

---
+++ client/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+
+      <div className={styles.center}>
+        <h1>Sound Page</h1>
+
+      </div>
+
+    </main>
+  );
+}
