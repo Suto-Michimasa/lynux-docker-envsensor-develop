55793a3871c2a52e3ef47bd3040b72f260e5f2f1
Suto <sutou1335michimasa@gmail.com>
2024-01-25 16:24:59 +0900

    add: 変更時のアニメーションを追加

--- client/src/app/components/sensorData.tsx
+++ client/src/app/components/sensorData.tsx
@@ -1,26 +1,41 @@
-'use client';
-
+'use client'
 import React, { useEffect, useState } from 'react';
 import { ref, child, get } from "firebase/database";
 import { db } from "@/lib/firebase/firebase";
+import './sensorData.css';
+// Define a type for your Firebase data
+interface SensorDataItem {
+  noise: number;
+  time: string;
+}
 
 interface SensorData {
-  [address: string]: {
-    noise: number;
-    time: string;
-  };
+  [address: string]: SensorDataItem;
+}
+
+interface AnimatedSensorData extends SensorData {
+  [address: string]: SensorDataItem & { animate: boolean };
 }
 
 const SensorData: React.FC = () => {
-  const [data, setData] = useState<SensorData>({});
+  const [data, setData] = useState<AnimatedSensorData>({});
 
   useEffect(() => {
     const fetchData = () => {
       const dbRef = ref(db);
       get(child(dbRef, 'sensor_data')).then((snapshot) => {
         if (snapshot.exists()) {
-          const data = snapshot.val();
-          setData(data);
+          // Use a type assertion here
+          const newData = snapshot.val() as SensorData;
+          const animatedData = Object.entries(newData).reduce((acc, [address, sensorData]) => {
+            // Check if data has changed to trigger animation
+            acc[address] = {
+              ...sensorData,
+              animate: data[address] && (data[address].noise !== sensorData.noise || data[address].time !== sensorData.time)
+            };
+            return acc;
+          }, {} as AnimatedSensorData);
+          setData(animatedData);
         } else {
           console.log("No data available");
         }
@@ -30,18 +45,18 @@ const SensorData: React.FC = () => {
     };
 
     fetchData();
-    const interval = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+    const interval = setInterval(fetchData, 5000);
 
-    return () => clearInterval(interval); // Clear interval on component unmount
-  }, []);
+    return () => clearInterval(interval);
+  }, [data]);
 
   return (
     <div>
       {Object.entries(data).map(([address, sensorData]) => (
-        <div key={address} style={{ marginBottom: '1rem' }}>
+        <div key={address} className={'sensor'}>
           <h2>{address}</h2>
-          <p>Noise: {sensorData.noise}</p>
-          <p>Time: {sensorData.time}</p>
+          <p className={sensorData.animate ? 'animate-text-change' : ''}>Noise: {sensorData.noise}</p>
+          <p className={sensorData.animate ? 'animate-text-change' : ''}>Time: {sensorData.time}</p>
         </div>
       ))}
     </div>
